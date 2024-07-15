1fc216c71e3ddb36fa36ffc577c1070acce8a69d
root <root@DESKTOP-T32DMP7>
2024-07-15 22:00:00 +0200

    first

--- fetch_flight_data.py
+++ fetch_flight_data.py
@@ -1,29 +1,159 @@
-# utf-8
-from  flightradar24 import FlightRadar24API
+from FlightRadar24 import FlightRadar24API
 from kafka import KafkaProducer
 import json
 import datetime
+import time
+import logging
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
-def get_flight_data():
-    fr_api = FlightRadar24API()
+# Configuration Kafka
+KAFKA_SERVER = "kafka:9092"
+FLIGHT_TOPIC = "flights"
+AIRLINE_TOPIC = "airlines"
+AIRPORT_TOPIC = "airports"
+ZONE_TOPIC = "zones"
+
+# Configuration logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def get_flights(fr_api):
+    logging.info("Fetching flight data")
     flights = fr_api.get_flights()
-    airports = fr_api.get_airports()
+    flights_to_dict = [
+        {
+            "id": flight.id,
+            "icao_24bit": flight.icao_24bit,
+            "latitude": flight.latitude,
+            "longitude": flight.longitude,
+            "heading": flight.heading,
+            "altitude": flight.altitude,
+            "ground_speed": flight.ground_speed,
+            "aircraft_code": flight.aircraft_code,
+            "registration": flight.registration,
+            "time": flight.time,
+            "origin_airport_iata": flight.origin_airport_iata,
+            "destination_airport_iata": flight.destination_airport_iata,
+            "number": flight.number,
+            "airline_iata": flight.airline_iata,
+            "on_ground": flight.on_ground,
+            "vertical_speed": flight.vertical_speed,
+            "callsign": flight.callsign,
+            "airline_icao": flight.airline_icao
+        }
+        for flight in flights
+    ]
+    logging.info(f"Fetched {len(flights_to_dict)} flight records")
+    return flights_to_dict
+
+def get_airlines(fr_api):
+    logging.info("Fetching airline data")
     airlines = fr_api.get_airlines()
+    airlines_to_dict = [
+        {"name": airline["Name"], "code": airline["Code"], "icao": airline["ICAO"]}
+        for airline in airlines
+    ]
+    logging.info(f"Fetched {len(airlines_to_dict)} airline records")
+    return airlines_to_dict
+
+def get_airports(fr_api):
+    logging.info("Fetching airport data")
+    airports = fr_api.get_airports()
+    airports_to_dict = [
+        {
+            "name": airport.name,
+            "iata": airport.iata,
+            "icao": airport.icao,
+            "altitude": airport.altitude,
+            "latitude": airport.latitude,
+            "longitude": airport.longitude
+        }
+        for airport in airports
+    ]
+    logging.info(f"Fetched {len(airports_to_dict)} airport records")
+    return airports_to_dict
+
+def extract_zones(zones):
+    zone_list = []
+    for zone_name, zone_data in zones.items():
+        if "subzones" in zone_data:
+            for subzone_name, subzone_data in zone_data["subzones"].items():
+                zone_list.append({
+                    "zone": zone_name,
+                    "subzone": subzone_name,
+                    "tl_y": subzone_data.get("tl_y"),
+                    "tl_x": subzone_data.get("tl_x"),
+                    "br_y": subzone_data.get("br_y"),
+                    "br_x": subzone_data.get("br_x")
+                })
+        else:
+            zone_list.append({
+                "zone": zone_name,
+                "subzone": None,
+                "tl_y": zone_data.get("tl_y"),
+                "tl_x": zone_data.get("tl_x"),
+                "br_y": zone_data.get("br_y"),
+                "br_x": zone_data.get("br_x")
+            })
+    return zone_list
+
+def get_zones(fr_api):
+    logging.info("Fetching zone data")
     zones = fr_api.get_zones()
+    zones_to_dict = extract_zones(zones)
+    logging.info(f"Fetched {len(zones_to_dict)} zone records")
+    return zones_to_dict
 
-    return {"flights": flights, "airports": airports, "airlines": airlines, "zones": zones}
+def send_to_kafka(producer, data, topic):
+    timestamp = datetime.datetime.now().isoformat()
+    for record in data:
+        message = {"timestamp": timestamp, "data": record}
+        producer.send(topic, value=message)
+    logging.info(f"Sent {len(data)} records to Kafka topic '{topic}'")
 
-def send_to_kafka(data, topic):
-    producer = KafkaProducer(
-        bootstrap_servers='kafka:9092',
-        value_serializer=lambda v: json.dumps(v).encode('utf-8')
-    )
-    producer.send(topic, data)
-    producer.flush()
 
 if __name__ == "__main__":
-    data = get_flight_data()
-    print(data)
-    timestamp = datetime.datetime.now().isoformat()
-    for data_type, records in data.items():
-        send_to_kafka({"timestamp": timestamp, "data": records}, f"flight_data_{data_type}")
+    logging.info("Starting Flight Data to Kafka script")
+
+    # Initialize FlightRadar24API
+    fr_api = FlightRadar24API()
+
+    # Initialize Spark session
+    spark = SparkSession.builder.appName("FlightRadar24ToParquet").getOrCreate()
+
+    # Kafka configuration
+    producer = KafkaProducer(
+        bootstrap_servers=KAFKA_SERVER,
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+
+    # Send zones data once
+    zones = get_zones(fr_api)
+    send_to_kafka(producer, zones, ZONE_TOPIC)
+
+    # Get and send airlines data
+    airlines = get_airlines(fr_api)
+    send_to_kafka(producer, airlines, AIRLINE_TOPIC)
+
+    # Get and send airports data
+    airports = get_airports(fr_api)
+    send_to_kafka(producer, airports, AIRPORT_TOPIC)
+
+    # Function to fetch and send flights data
+    def fetch_and_send_data():
+        flights = get_flights(fr_api)
+        send_to_kafka(producer, flights, FLIGHT_TOPIC)
+
+    # Fetch and send flights data every minute
+    while True:
+        fetch_and_send_data()
+        logging.info("Sleeping for 1 minute")
+        time.sleep(60)  # Wait for 1 minute
+
+    # Clean up
+    producer.flush()
+    producer.close()
+    spark.stop()
+    logging.info("Stopped Flight Data to Kafka script")
+
--- spark_streaming.py
+++ spark_streaming.py
@@ -1,29 +1,142 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import year, month, dayofmonth, hour, current_timestamp
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
-import json
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
+# Configuration Kafka
 KAFKA_SERVER = "kafka:9092"
-TOPICS = ["flight_data_flights", "flight_data_airports", "flight_data_airlines", "flight_data_zones"]
-STORAGE_PATH = "/data/flight_data/"
+STORAGE_PATH = "/app/data"  # Chemin de sauvegarde pour Parquet
 
-spark = SparkSession.builder.appName("FlightDataStreaming").getOrCreate()
-ssc = StreamingContext(spark.sparkContext, 10)  # 10-second batches
+# Initialisation de la session Spark
+spark = SparkSession.builder \
+    .appName("KafkaToParquet") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1") \
+    .getOrCreate()
 
-kafka_streams = [KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": KAFKA_SERVER}) for topic in TOPICS]
+# Schéma pour le topic "flights"
+flight_schema = StructType([
+    StructField("timestamp", StringType(), True),
+    StructField("data", StructType([
+        StructField("id", StringType(), True),
+        StructField("icao_24bit", StringType(), True),
+        StructField("latitude", StringType(), True),
+        StructField("longitude", StringType(), True),
+        StructField("heading", StringType(), True),
+        StructField("altitude", StringType(), True),
+        StructField("ground_speed", StringType(), True),
+        StructField("aircraft_code", StringType(), True),
+        StructField("registration", StringType(), True),
+        StructField("time", TimestampType(), True),  
+        StructField("origin_airport_iata", StringType(), True),
+        StructField("destination_airport_iata", StringType(), True),
+        StructField("number", StringType(), True),
+        StructField("airline_iata", StringType(), True),
+        StructField("on_ground", StringType(), True),
+        StructField("vertical_speed", StringType(), True),
+        StructField("callsign", StringType(), True),
+        StructField("airline_icao", StringType(), True)
+    ]))
+])
 
-def process(rdd, data_type):
-    if not rdd.isEmpty():
-        df = spark.read.json(rdd.map(lambda x: x[1]))
-        df = df.withColumn("year", year(current_timestamp())) \
-               .withColumn("month", month(current_timestamp())) \
-               .withColumn("day", dayofmonth(current_timestamp())) \
-               .withColumn("hour", hour(current_timestamp()))
-        df.write.partitionBy("year", "month", "day", "hour").parquet(f"{STORAGE_PATH}{data_type}")
+# Schéma pour le topic "airline"
+airline_schema = StructType([
+    StructField("timestamp", StringType(), True),
+    StructField("data", StructType([
+        StructField("name", StringType(), True),
+        StructField("code", StringType(), True),
+        StructField("icao", StringType(), True)
+    ]))
+])
 
-for kafka_stream, data_type in zip(kafka_streams, ["flights", "airports", "airlines", "zones"]):
-    kafka_stream.foreachRDD(lambda rdd: process(rdd, data_type))
+# Schéma pour le topic "zone"
+zone_schema = StructType([
+    StructField("timestamp", StringType(), True),
+    StructField("data", StructType([
+        StructField("zone", StringType(), True),
+        StructField("subzone", StringType(), True),
+        StructField("tl_y", DoubleType(), True),
+        StructField("tl_x", DoubleType(), True),
+        StructField("br_y", DoubleType(), True),
+        StructField("br_x", DoubleType(), True)
+    ]))
+])
+# Schéma pour le topic "airport"
+airport_schema = StructType([
+    StructField("timestamp", StringType(), True),
+    StructField("data", StructType([
+        StructField("name", StringType(), True),
+        StructField("iata", StringType(), True),
+        StructField("icao", StringType(), True),
+        StructField("altitude", StringType(), True),
+        StructField("latitude", StringType(), True),
+        StructField("longitude", StringType(), True),
 
-ssc.start()
-ssc.awaitTermination()
+
+    ]))
+])
+
+# Fonction pour lire depuis Kafka et écrire en Parquet avec partitionnement pour "flights"
+def kafka_to_parquet_partitioned(topic, schema):
+    # Lecture en continu des données depuis Kafka
+    df = spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", KAFKA_SERVER) \
+        .option("subscribe", topic) \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+    # Conversion de la valeur du message Kafka en colonnes structurées
+    df_processed = df.selectExpr("CAST(value AS STRING)") \
+                     .select(from_json(col("value"), schema).alias("parsed_data")) \
+                     .select("parsed_data.data.*")
+    df_processed = df_processed.withColumn("year", year("time")) \
+                               .withColumn("month", month("time")) \
+                               .withColumn("day", dayofmonth("time")) \
+                               .withColumn("hour", hour("time"))
+
+    # Écriture en continu des données dans un répertoire Parquet partitionné
+    query = df_processed.writeStream \
+        .format("parquet") \
+        .option("path", f"{STORAGE_PATH}/{topic}") \
+        .option("checkpointLocation", f"{STORAGE_PATH}/{topic}_checkpoint") \
+        .partitionBy("year", "month", "day", "hour") \
+        .outputMode("append") \
+        .start()
+
+    return query
+
+# Fonction pour lire depuis Kafka et écrire en Parquet sans partitionnement pour "airline", "zone", "airport"
+def kafka_to_parquet_one_shot(topic, schema):
+    # Lecture des données depuis Kafka
+    df = spark.read \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", KAFKA_SERVER) \
+        .option("subscribe", topic) \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+    # Conversion de la valeur du message Kafka en colonnes structurées
+    df_processed = df.selectExpr("CAST(value AS STRING)") \
+                     .select(from_json(col("value"), schema).alias("parsed_data")) \
+                     .select("parsed_data.data.*")
+
+    # Écriture des données dans un répertoire Parquet
+    df_processed.write \
+        .format("parquet") \
+        .mode("overwrite") \
+        .option("path", f"{STORAGE_PATH}/{topic}") \
+        .save()
+
+# Lancement du flux pour les topics spécifiés
+query_flights = kafka_to_parquet_partitioned("flights", flight_schema)
+
+# Lancer les traitements one-shot pour "airline", "zone", et "airport"
+kafka_to_parquet_one_shot("airlines", airline_schema)
+kafka_to_parquet_one_shot("zones", zone_schema)
+kafka_to_parquet_one_shot("airports", airport_schema)
+
+# Attente de la terminaison du flux pour "flights"
+query_flights.awaitTermination()
+
+# Arrêt de la session Spark
+spark.stop()
+
